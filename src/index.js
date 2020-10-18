48555b47362bc699d9eb31539e7dea1425b9b2ed
Massimiliano Rizzuto <massimiliano.rizzuto87@gmail.com>
2020-10-18 15:50:45 +0200

    chore: add fontawesome for icons in the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import helloWorld from './js/app.js';
+
 import 'bootstrap/dist/css/bootstrap-reboot.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import './style/app.css';
 
 helloWorld();
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 function helloWorld() {
   const app = document.getElementById('app');
   const title = document.createElement('h1');
+  const icon = document.createElement('i');
 
+  icon.classList.add('fas', 'fa-globe');
   title.textContent = 'Hello World!';
+  app.appendChild(icon);
   app.appendChild(title);
 }
 
